b4904f5887fbb85fe865087ada7ee1756f8a16da
Alexis Mejia <mejia099@csusm.edu>
2019-07-25 16:25:29 -0700

    Almost finished the tic tac toe game

--- Welearn/M2-javaScript/L3-JSObject-Array/Console/script.js
+++ Welearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -2,4 +2,49 @@ const article = {
   name: 'Dog family gives burth to 10 puppoes',
   views: 1035,
   datePublised: '03/25/2018',
+
+  author: {
+    name: 'Joe Corgi',
+    title: 'Senior Canine Editor'
+  }
 };
+
+const favorite = {
+  snack: 'Apples',
+  favNumb: 7,
+  number3: 'Tacos',
+  author: 'r.h Sin',
+  name:  'Joe Corgi',
+  title: 'Senior Canine Editor'
+};
+
+console.log(favorite.snack);
+console.log(favorite.favNumb);
+console.log(favorite.number3);
+console.log(favorite.author);
+console.log(favorite.name);
+console.log(favorite.title);
+
+const foods = {
+  1:'mexican',
+  2:'chinese',
+  3:'american',
+  4:'colombian',
+  5:'german',
+  6:'mre',
+}
+ console.log(foods);
+
+ const seasonns = ['Spring','Summer', 'Fall','Winter'];
+ const friends = ['Alice','Bob', 'Charile'];
+ const numbers = [5, 10, 2, 16, 50];
+
+ const classes = [];
+ classes.push({course: 'CS 111', title: 'Computer Science I'});
+ classes.push({course: 'Math 160', title: 'Calculus I'});
+
+ const party = {
+   date: '7/4/2018',
+   location: 'Library',
+   guest: ['Mary', 'Joe', 'Smith']
+ };
---
+++ Welearn/M2-javaScript/Labs/lab2-tictactoe/ttt.js
@@ -0,0 +1,178 @@
+let turn = 0; // default player one
+let p1 = [ ];
+let p2 = [ ];
+
+
+
+const b1 = document.querySelector('#b1');
+b1.addEventListener('click', (e) => {
+  console.log(p1[]);
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+  //console.log(turn);
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b1.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b1.style.backgroundColor = 'red';//working
+  }
+})
+
+//button.addEventListener
+const b2 = document.querySelector('#b2');
+b2.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b2.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b2.style.backgroundColor = 'red';//working
+  }
+})
+
+const b3 = document.querySelector('#b3');
+b3.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b3.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b3.style.backgroundColor = 'red';//working
+  }
+})
+
+const b4 = document.querySelector('#b4');
+b4.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b4.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b4.style.backgroundColor = 'red';//working
+  }
+})
+
+const b5 = document.querySelector('#b5');
+b5.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b5.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b5.style.backgroundColor = 'red';//working
+  }
+})
+
+const b6 = document.querySelector('#b6');
+b6.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b6.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b6.style.backgroundColor = 'red';//working
+  }
+})
+
+const b7 = document.querySelector('#b7');
+b7.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b7.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b7.style.backgroundColor = 'red';//working
+  }
+})
+
+const b8 = document.querySelector('#b8');
+b8.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b8.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b8.style.backgroundColor = 'red';//working
+  }
+})
+
+const b9 = document.querySelector('#b9');
+b9.addEventListener('click', (e) => {
+  console.log("click");
+
+  turn++;
+  if (turn === 2)
+  {
+    turn = 0;
+  }
+
+  //b1.style.backgroundColor = 'blue';//working
+  if (turn === 0)
+  {
+    b9.style.backgroundColor = 'blue';//working
+  }else if (turn === 1){
+    b9.style.backgroundColor = 'red';//working
+  }
+})
