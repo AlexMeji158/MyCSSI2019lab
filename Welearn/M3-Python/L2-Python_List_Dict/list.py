1327d417f9ac9f53face23dc6f1660eae977543a
Alexis Mejia <mejia099@csusm.edu>
2019-07-31 11:57:19 -0700

    We are about to work on the challenge.

---
+++ Welearn/M3-Python/L2-Python_List_Dict/list.py
@@ -0,0 +1,59 @@
+#################### Access elements on a list #############
+'''
+students = ["Alice", "Javi", "Damien"]
+students.append("Delilah")
+students.insert(1,"Zoe")
+students.remove("Javi")
+print(students)
+'''
+############ List iteration by Element #############
+'''
+smith_sibilings = ["Emily", "Monique","Giovanni"]
+for name in smith_sibilings:
+    print(name + "Smith")
+'''
+'''
+x = 0
+smith_sibilings2 = ["Emily", "Monique","Giovanni","Logan", "Melissa","Micheal","Alex"]
+for name in smith_sibilings2:
+    x = x + 1
+    print(name + "Smith")
+    print(x)
+'''
+'''
+smith_sibilings3 = ["Emily", "Monique","Giovanni"]
+students.append("Delilah")
+students.append("Alex")
+students.append("John")
+students.append("Brian")
+
+for name in smith_sibilings3:
+    print(name + "Smith")
+    print(len(smith_sibilings3))
+'''
+'''
+smith_sibilings4 = ["Emily","Monique","Giovanni","Logan", "Melissa"]
+for index in range(len(smith_sibilings4)):
+    smith_sibilings4[index] = smith_sibilings4[index] + "Smith"
+
+print(smith_sibilings4)
+'''
+################### Checking list Membership #################
+'''
+superheroes = ["Captain Marvel","Wonder Woman","Storm","Kamala Khan","Bumble Bee","Jessica Jones"]
+
+demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5?"))
+
+if demoted_hero in superheroes:
+    superheroes.remove(demoted_hero)
+    print("Top 5 heroes:", superheroes)
+else:
+    print("Hero not found.")
+'''
+############### Slicing ######################
+
+names = ["Rickon","Bran","Arya","Sansa","Jon","Robb"]
+print (names[::-1]) # prints the list in reverse
+print (names[4:2:-1]) # Only prints idx 4 and reverse idx 2, prints in reverse
+print (names[:2]) # prints only the first 2 idx
+print (names[::2]) # Skips every other
